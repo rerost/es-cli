4500c80d0514285b1ec73d43a399770798e05573
rerost <gmailii@yahoo.co.jp>
2019-06-21 01:20:46 +0900

    Fix delete

--- cmd/delete/index/index.go
+++ cmd/delete/index/index.go
@@ -2,8 +2,6 @@ package delete
 
 import (
 	"context"
-	"io"
-	"os"
 
 	"github.com/rerost/es-cli/domain"
 	"github.com/spf13/cobra"
@@ -14,22 +12,9 @@ func NewIndexCmd(ctx context.Context, ind domain.Index) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "index",
 		Short: "delete index",
-		Args:  cobra.RangeArgs(0, 1),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(_ *cobra.Command, args []string) error {
-			var fp io.Reader
-			switch len(args) {
-			case 1:
-				fp = os.Stdin
-			case 2:
-				// Read file from filename
-				fileName := args[1]
-				var err error
-				fp, err = os.Open(fileName)
-				if err != nil {
-					return fail.Wrap(err)
-				}
-			}
-			err := ind.Create(ctx, args[0], fp)
+			err := ind.Delete(ctx, args[0])
 			if err != nil {
 				return fail.Wrap(err)
 			}
